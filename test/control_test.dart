ecde90f3f58fb3899bde41a5db702566cc42f9df
Alejandro Pedrero <alejandro.pedrero@perficient.com>
2026-03-20 12:27:07 -0600

    test: Agregando dummy test para habilitar el pipeline de CI/CD

---
+++ test/control_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Prueba de control inicial (Dummy Test) para CI/CD', () {
+    // Si 1 + 1 es igual a 2, la prueba pasa y el robot de GitHub continúa.
+    expect(1 + 1, 2);
+  });
+}
